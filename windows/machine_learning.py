c8e25d42d71da15d5808ac90b96537ae6fa85e68
closedeyesignal <apox_117@hotmail.com>
2020-12-12 12:03:51 -0700

    Moved destroy event into separate function in order to update master property containing its reference

--- windows/machine_learning.py
+++ windows/machine_learning.py
@@ -8,14 +8,19 @@ class MachineLearning(tk.Toplevel):
         self.geometry("600x400")
         self.title("Machine Learning")
         self.minsize(400, 300)
+        self.protocol("WM_DELETE_WINDOW", self.destroy_window)
 
         self.menubar = tk.Menu(self)
         filemenu = tk.Menu(self.menubar, tearoff=0)
         filemenu.add_command(label="Import data", command=self.donothing)
         filemenu.add_command(label="Export data", command=self.donothing)
         filemenu.add_separator()
-        filemenu.add_command(label="Close", command=self.destroy)
+        filemenu.add_command(label="Close", command=self.destroy_window)
         self.menubar.add_cascade(label="File", menu=filemenu)
 
     def donothing(self):
          x = 0
+
+    def destroy_window(self):
+        self.master.windows.pop(MachineLearning)
+        self.destroy()
